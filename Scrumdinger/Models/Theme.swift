3527a9c182a3fa62098c801002f2b85c65e46ac2
na8esin <t-watanabe@watanabetakayukinoMacBook-Pro.local>
2024-07-24 21:49:11 +0900

    chapter 4 ended.

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+enum Theme: String, CaseIterable, Identifiable {
+  case bubblegum
+  case buttercup
+  case indigo
+  case lavender
+  case magenta
+  case navy
+  case orange
+  case oxblood
+  case periwinkle
+  case poppy
+  case purple
+  case seafoam
+  case sky
+  case tan
+  case teal
+  case yellow
+  
+  var accentColor: Color {
+    switch self {
+    case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+    case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+    }
+  }
+  var mainColor: Color {
+    // https://developer.apple.com/documentation/swiftui/color/init(_:bundle:)
+    // Asset Catalogに設定がないといけない
+      Color(rawValue)
+  }
+  var name: String {
+    rawValue.capitalized
+  }
+  var id: String {
+    name
+  }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+  @State private var scrums = DailyScrum.sampleData
+
+  var body: some Scene {
+      WindowGroup {
+        ScrumsView(scrums: $scrums)
+      }
+  }
+}
